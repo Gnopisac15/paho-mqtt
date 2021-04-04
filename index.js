822753b067e1fb2ed407dd201d24f8c15cb2d92f
Gnopisac15 <melchorcasipongjr@gmail.com>
2021-04-04 16:27:04 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,54 +1,5 @@
 console.log("index.js");
 
-// // var client  = mqtt.connect({ host:'test.mosquitto.org', port: 8081})
-// // or
-// var client = mqtt.connect('wss://test.mosquitto.org:8081/mqtt')
-
-// // var client  = mqtt.connect({ host:'mqtt.eclipse.org/mqtt', port: 443})
-// // or
-// // var client  = mqtt.connect('wss://mqtt.eclipse.org:443/mqtt')
-
-
-// //Publish
-// var pub_button = document.getElementById('pub-button');
-// var pub_topic = document.getElementById('pub-input-topic');
-// var pub_payload = document.getElementById('pub-input-payload');
-
-// //Subscribe
-// var sub_topic = document.getElementById('sub-input-topic');
-// var sub_button = document.getElementById('sub-button');
-
-
-
-//Creating table row 
-// function myFunctionTable() {
-//   var tbody = document.getElementById('incoming');
-//   var row = tbody.insertRow(0)
-//   var cell1 = row.insertCell(0);
-//   var cell2 = row.insertCell(1);
-//   cell1.innerHTML = pub_topic.value;
-//   cell2.innerHTML = pub_payload.value;
-// }
-
-
-// client.on('message', function (topic, payload) {
-//   myFunctionTable();
-// })
-
-
-// //Publish button event
-// pub_button.addEventListener('click', () => {
-//   // console.log('clicked');
-//   // console.log(pub_input.value);
-//   client.publish(pub_topic.value, pub_payload.value);
-// })
-// //Subscribe button event
-// sub_button.addEventListener('click', () => {
-//   client.subscribe(sub_topic.value);
-//   console.log(sub_topic.value)
-// })
-
-
 var address = document.getElementById('broker-address').value;
 var broker_btn = document.getElementById('broker-btn-con');
 var pub_button = document.getElementById('pub-btn');
@@ -110,14 +61,24 @@ broker_btn.addEventListener('click', () => {
     document.getElementById('broker-status').value = "Connected!"
 
     pub_button.addEventListener('click', () => {
-      client.publish(pub_topic.value, pub_payload.value);
-      myFunctionTablePublish();
+      if (pub_topic.value != "" && pub_payload.value != "") {
+        client.publish(pub_topic.value, pub_payload.value);
+        myFunctionTablePublish();
+      } else {
+        alert("Please fill out all field!")
+      }
+
     })
     //Subscribe button event
     sub_button.addEventListener('click', () => {
-      client.subscribe(sub_topic.value);
-      myFunctionTableSubscriber();
-        
+
+      if (sub_topic.value != "") {
+        client.subscribe(sub_topic.value);
+        myFunctionTableSubscriber();
+      } else {
+        alert("Please fill out all field!")
+      }
+
     })
     myFunctionTableBroker()
   })
